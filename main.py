86b2418d7e7aca8b78faa5e2596d2193319e287a
omerh <omerha@protonmail.com>
2023-10-20 10:04:58 +0300

    updates

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import sdnotify
 import time
 
-print("Test starting up...")
-# In a real service, this is where you'd do real startup tasks
-# like opening listening sockets, connecting to a database, etc...
-time.sleep(10)
-print("Test startup finished")
 
-# Inform systemd that we've finished our startup sequence...
-n = sdnotify.SystemdNotifier()
-n.notify("READY=1")
+def main():
+    # Inform systemd that we've finished our startup sequence...
+	n = sdnotify.SystemdNotifier()
+	n.notify("READY=1")
 
-count = 1
-while True:
-	print("Running... {}".format(count))
-	n.notify("STATUS=Count is {}".format(count))
-	count += 1
-	time.sleep(2)
+	count = 1
+	while True:
+		print("Running... {}".format(count))
+		n.notify("STATUS=Count is {}".format(count))
+		count += 1
+		time.sleep(2)
+
+
+if __name__ == "__main__":
+    print("Test starting up...")
+    main()
+
