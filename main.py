16c9cf07ad473be20bdca585a368e3c41095057b
omerh <omerha@protonmail.com>
2023-10-20 10:17:37 +0300

    added logger to journal

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import sdnotify
 import time
+import logging
+from systemd import journal
+
+log = logging.getLogger('demo')
+log.addHandler(journal.JournaldLogHandler())
+log.setLevel(logging.INFO)
 
 
 def main():
     # Inform systemd that we've finished our startup sequence...
-	n = sdnotify.SystemdNotifier()
-	n.notify("READY=1")
+	# n = sdnotify.SystemdNotifier()
+	log.info("READY=1")
 
 	count = 1
 	while True:
-		print("Running... {}".format(count))
-		n.notify("STATUS=Count is {}".format(count))
+		log.info("Running... {}".format(count))
+		log.info("STATUS=Count is {}".format(count))
 		count += 1
 		time.sleep(2)
 
 
 if __name__ == "__main__":
-    print("Test starting up...")
+    log.info("Test starting up...")
     main()
 
